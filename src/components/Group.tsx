4f2605169b2ea747e71eb659c4899e25357c3113
LouisG2025 <louis.grange@apexai.ae>
2026-03-03 17:28:23 +0530

    update

---
+++ src/components/Group.tsx
@@ -0,0 +1,14 @@
+import GroupIcon from '../assets/GroupIcon.svg';
+
+const Group = () => {
+    return (
+        <img
+            src={GroupIcon}
+            alt="Group design"
+            className="relative w-full max-w-full overflow-hidden"
+            style={{ height: '1651.5px' }}
+        />
+    );
+};
+
+export default Group;
